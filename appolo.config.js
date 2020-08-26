52cf9725c89ac64c9a707a8a7abe3a9718e7e0f5
Wahbi <hwahbi@dxc.com>
2020-08-26 10:16:46 -0500

    update

--- appolo.config.js
+++ appolo.config.js
@@ -2,7 +2,7 @@ module.exports = {
     client: {
       service: {
         name: "trakbak",
-        url: "http:/35.184.87.22:3001/graphql"
+        url: "http://35.184.87.22:3001/graphql"
 
       }
     }
